933282d4d16be95006297a59f2632e2651287bda
Murad <mur1d1xmedov@gmail.com>
2022-11-30 04:37:57 +0400

    created components

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+import Form from "./Form";
+
+function Contacts() {
+  return (
+    <>
+      <List />
+      <Form />
+    </>
+  );
+}
+
+export default Contacts;
